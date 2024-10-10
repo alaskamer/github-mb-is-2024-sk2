3dc027d08a6dbd8cb3b589e4e50698f9df8dd8c6
alaskamer <marie.borisjukova@tul.cz>
2024-10-10 13:34:00 +0200

    Mid work commit v2

---
+++ IS-project/program001-vypis-rady/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program {
+
+    static void Main() {
+
+            //chci, aby ze program opakoval po stisku klavesy "a"
+
+        string again = "a";
+        while (again == "a") {
+            Console.Clear();
+            Console.WriteLine("**************************");
+            Console.WriteLine("*******vzpis rady*********");
+            Console.WriteLine("**************************");
+            Console.WriteLine("*******Marie Borisjukova**");
+            Console.WriteLine("************************/n/n");
+            Console.WriteLine();
+
+            // vstup do uzivatele - TO DO
+
+
+            // Logika pro vypis rady - TO DO
+
+
+            // opakovani programu 
+            Console.WriteLine("pro opakovani programu stisknete klavesu a");
+            again = Console.ReadLine();
+        }
+
+
+    }
+}
